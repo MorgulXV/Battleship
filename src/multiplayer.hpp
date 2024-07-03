f46f86096b60eb8ae3cbce4a463dc0b8f9eac382
MorgulXV <108887181+MorgulXV@users.noreply.github.com>
2024-07-03 17:54:38 +0200

    Update multiplayer.hpp

--- src/multiplayer.hpp
+++ src/multiplayer.hpp
@@ -2,3 +2,10 @@
 #include <unistd.h>
 #include <cstdio.h>
 #include <atomic.h>
+
+struct Packet{
+    unsigned char x : 4;
+    unsigned char y : 4;
+    unsigned char isShip : 1;
+    unsigned char isSunken : 1;
+};
