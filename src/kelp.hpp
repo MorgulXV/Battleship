f0dc49fcb2ed2d7e56a5a381b8706db9199b5e67
MorgulXV <timtueftel@icloud.com>
2024-07-11 16:45:42 +0200

    Update kelp.hpp

--- src/kelp.hpp
+++ src/kelp.hpp
@@ -6,7 +6,7 @@
 #include "setup.hpp"
 #include "input.hpp"
 
-std::array<Ship, 5> searchAdjacentShips(const std::array<std::array<Field, MAX_X>, MAX_Y> Board){
+void(const std::array<std::array<Field, MAX_X>, MAX_Y> Board){
     for(int j = 0; j < 10; j++){
         for(int k = 0; k < 10; k++){
             if(Board[j][k].isShip == true){
